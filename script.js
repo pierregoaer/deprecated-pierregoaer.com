1163e5c49e887be643aa1bef2a08e1108c35e407
pierregoaer <hello@pierregoaer.com>
2022-01-08 16:26:08 -0500

    new dark mode animation

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const toggle = document.querySelector('.toggle-container');
 const body = document.querySelector('body');
+const siteWrapper = document.querySelector('.site-wrapper');
+const darkmodeElement = document.querySelector('.darkmode-element');
 
 //Turn on/off Dark Mode
-// Initialize dark mode on first load (was too lazy to change that initially)
-body.classList.add('dark-mode-on');
+// Initialize dark mode on first load
+// body.classList.add('dark-mode-on');
+siteWrapper.classList.add('dark-mode-on');
+darkmodeElement.classList.add('dark-mode-on');
 
 toggle.onclick = () => {
-	// body.style.backgroundColor === 'var(--dark-background)' ? (body.style.backgroundColor = 'var(--light-background)') : (body.style.backgroundColor = 'var(--dark-background)');
-	body.classList.toggle('dark-mode-on');
+	// body.classList.toggle('dark-mode-on');
+	darkmodeElement.classList.toggle('dark-mode-on');
+
+	// Have all the other elements change colour after propagation of the dark mode element
+	setTimeout(() => siteWrapper.classList.toggle('dark-mode-on'), 800);
+
+	// Tried using transition end but it's firing multiple times and the same type everytime
+	// darkmodeElement.addEventListener('transitionend', e => {
+	// 	console.log(e);
+	// siteWrapper.classList.toggle('dark-mode-on');
+	// });
 };
