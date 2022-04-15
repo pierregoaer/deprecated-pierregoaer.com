3d209610d13a83df43a7c47b5d1ab7cbef3d7e0f
pierregoaer <hello@pierregoaer.com>
2022-04-15 14:04:33 -0400

    Fixing mobile menu navigation

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const darkmodeElement = document.querySelector('.darkmode-element');
 // darkmodeElement.classList.add('dark-mode-on');
 
 toggle.onclick = () => {
+	console.log('Change light');
 	body.classList.toggle('dark-mode-on');
 	// darkmodeElement.classList.toggle('dark-mode-on');
 
@@ -20,13 +21,26 @@ toggle.onclick = () => {
 	// }, 800);
 };
 
-const mobileNavIiconContainer = document.querySelector('.mobile-nav-icon-container');
+const mobileNavIconContainer = document.querySelector('.mobile-nav-icon-container');
 const mobileNavIcon = document.querySelectorAll('.mobile-nav-icon');
 const mainNavigation = document.querySelector('.main-navigation');
+const mainNavigationLinks = document.querySelectorAll('.main-navigation a');
 
-mobileNavIiconContainer.addEventListener('click', function () {
+mobileNavIconContainer.addEventListener('click', function () {
+	mobileNavIconContainer.classList.toggle('active');
 	mobileNavIcon.forEach(bar => bar.classList.toggle('active'));
 	const visibility = mainNavigation.getAttribute('data-visible');
 	if (visibility === 'false') mainNavigation.setAttribute('data-visible', true);
 	if (visibility === 'true') mainNavigation.setAttribute('data-visible', false);
 });
+
+mainNavigationLinks.forEach(
+	link =>
+		(link.onclick = () => {
+			mobileNavIcon.forEach(bar => bar.classList.remove('active'));
+			mobileNavIconContainer.classList.remove('active');
+			const visibility = mainNavigation.getAttribute('data-visible');
+			if (visibility === 'false') mainNavigation.setAttribute('data-visible', true);
+			if (visibility === 'true') mainNavigation.setAttribute('data-visible', false);
+		})
+);
